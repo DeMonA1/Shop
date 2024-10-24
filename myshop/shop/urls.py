572c2460e942b1cf1d24a37b422800faf2926f38
DeMonA1 <demonagrail1@gmail.com>
2024-10-24 17:29:52 +0300

    edit

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -6,6 +6,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    # for get_absolute_url in the Category model
     path('<slug:category_slug>/', views.product_list, 
         name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
