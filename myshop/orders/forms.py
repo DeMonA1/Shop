02795f7043110172ae7417b0f69e1eddbf9138ee
DeMonA1 <demonagrail1@gmail.com>
2024-10-26 15:55:45 +0300

    edit

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -6,6 +6,6 @@ class OrderCreateForm(forms.ModelForm):
     
     class Meta:
         model = Order
-        fields = ['firstname', 'last_name',
+        fields = ['first_name', 'last_name',
                   'email', 'address',
                   'postal_code', 'city']
