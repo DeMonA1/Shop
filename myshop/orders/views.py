669a24cd9b3539d123bf0e918d6a023a09c40ec2
DeMonA1 <demonagrail1@gmail.com>
2024-10-28 19:33:50 +0300

    add task to the queue

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from cart.cart import Cart
+from celery import Task
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 # Create your views here.
 
@@ -18,6 +20,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
