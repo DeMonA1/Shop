311c7387cb02eca5f4d30b820967d765fdec4139
DeMonA1 <demonagrail1@gmail.com>
2024-10-31 10:45:17 +0300

    payment_canceled and ..._completed views

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -38,3 +38,13 @@ def payment_process(request):
         return render(request,
                       'payment/process.html',
                       locals())
+        
+        
+def payment_completed(request):
+    return render(request,
+                  'payment/completed.html')
+    
+    
+def payment_canceled(request):
+    return render(request,
+                  'payment/canceled.html')
